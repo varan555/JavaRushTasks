47fbb0afdf995f266f9cadcc5659d0bf2a96e652
varan555 <36423268+varan555@users.noreply.github.com>
2020-05-11 18:37:18 +0300

    Add files via upload

---
+++ task3305/ParkingLot.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task33.task3305;
+
+import java.util.List;
+
+public class ParkingLot {
+    public String name;
+    public String city;
+    public List<Vehicle> vehicles;
+
+    public ParkingLot(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    @Override
+    public String toString() {
+        return "ParkingLot{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", vehicles=" + vehicles +
+                '}';
+    }
+}
---
+++ task3305/Vehicle.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task33.task3305;
+
+public abstract class Vehicle {
+    protected String name;
+    protected String owner;
+    protected int age;
+}
